63095b2c050f85d5e1348c1973959529cd3acde0
ZenonStella <stellazenon@gmail.com>
2022-05-17 11:30:23 +0200

    reset post

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,29 @@ let footer = document.getElementById('footer');
 let title = document.getElementById('title');
 let posts = document.getElementById('posts')
 let verifForm = true;
-let myArray = []
-let articlesStr = localStorage.getItem("mesArticles");
-let articles = articlesStr.split(',')
 let countPosts = 0;
+let randomColor
+let articles = []
+if (localStorage.getItem('mesArticles')) {
+    articles = localStorage.getItem("mesArticles");
+    articles = articles.split(',')
+
+
+}
+articles.forEach(article => {
+    randomColor = Math.floor(Math.random() * 5);
+    console.log(article);
+    posts.innerHTML += `
+        <div id="post${countPosts}" class="shadow width pastel${randomColor} raduis d-flex flex-column align-items-center m-2 p-3">
+            <div class="text">
+                <p>${article}</p>
+            </div>
+            <button class="btn btn-danger" onclick="resetPost('post${countPosts}')">Supprimer</button>
+        </div>
+    `;
+    countPosts++
+});
+console.log(articles);
 let colors = [
     'rgb(124, 252, 252)',
     'rgb(251, 154, 220)',
@@ -19,24 +38,13 @@ let randomFooter = Math.floor(Math.random() * 5);
 header.style.backgroundColor = colors[randomHeader];
 footer.style.backgroundColor = colors[randomFooter];
 
-function show() {
-    articles.forEach(article => {
-        let randomColor = Math.floor(Math.random() * 5);
-        console.log(article);
-        posts.innerHTML += `
-            <div id="post${countPosts}" class="shadow width pastel${randomColor} raduis d-flex flex-column align-items-center m-2 p-3">
-                <div class="text">
-                    <p>${article}</p>
-                </div>
-                <button class="btn btn-danger" onclick="resetPost('post${countPosts}')">Supprimer</button>
-            </div>
-        `;
-        countPosts++
-    });
-}
+// function show() {
+
+// }
 
 function getForm() {
     let titleError = document.getElementById('titleError');
+    randomColor = Math.floor(Math.random() * 5);
 
     if (title.value == '') {
         titleError.innerHTML = '* Veuillez saisir un titre';
@@ -46,10 +54,20 @@ function getForm() {
         verifForm = true;
     }
     if (verifForm == true) {
-        myArray.push(title.value)
-        localStorage.setItem("mesArticles", myArray)
+        articles.push(title.value)
+        console.log(articles);
+        posts.innerHTML += `
+            <div id="post${countPosts}" class="shadow width pastel${randomColor} raduis d-flex flex-column align-items-center m-2 p-3">
+                <div class="text">
+                    <p>${title.value }</p>
+                </div>
+                <button class="btn btn-danger" onclick="resetPost('post${countPosts}')">Supprimer</button>
+            </div>
+        `;
+        countPosts++
+        localStorage.setItem("mesArticles", articles)
         title.value = '';
-        console.log(myArray);
+
     }
 
 }
@@ -61,6 +79,7 @@ function clearError(id) {
 
 function resetPost(id) {
     let post = document.getElementById(id)
+    articles.splice(post, 1)
     post.remove()
     localStorage.setItem("mesArticles", articles)
 
